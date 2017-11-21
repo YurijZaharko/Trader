3ea973ef7022f1ae84598328009ea2853513f16f
YurijZaharko <YurijZaharko@gmail.com>
2017-11-21 16:24:43 +0200

    refactoring validator

--- src/main/java/project/form/validator/RegistrationValidator.java
+++ src/main/java/project/form/validator/RegistrationValidator.java
@@ -11,7 +11,6 @@ import java.util.regex.Pattern;
 public class RegistrationValidator implements Validator {
 
     private String emailPattern;
-
     private String nickNamePattern;
 
     @Override
@@ -22,21 +21,41 @@ public class RegistrationValidator implements Validator {
     @Override
     public void validate(Object o, Errors errors) {
         RegistrationForm registrationForm = (RegistrationForm) o;
-        Pattern email = Pattern.compile(emailPattern);
-        Pattern nickName = Pattern.compile(nickNamePattern);
+        emailMatcher(errors,registrationForm);
+        nickNameMatcher(errors, registrationForm);
+        passwordMatcher(errors,registrationForm);
+        validateEmptyOrWhitespace(errors);
+    }
 
-        if (!email.matcher(registrationForm.getEmail()).matches()) {
+    private void emailMatcher(Errors errors, RegistrationForm registrationForm){
+        Pattern emailCompilePattern = Pattern.compile(emailPattern);
+        String email = registrationForm.getEmail();
+        if (!emailCompilePattern.matcher(email).matches()) {
             errors.rejectValue("email", "", "Email must contain only a-z A-Z 0-9 _ + * / -");
         }
+    }
 
-        if (!nickName.matcher(registrationForm.getNickName()).matches()) {
+    private void nickNameMatcher(Errors errors, RegistrationForm registrationForm){
+        Pattern nickNameCompilePattern = Pattern.compile(nickNamePattern);
+        String nickName = registrationForm.getNickName();
+        if (!nickNameCompilePattern.matcher(nickName).matches()) {
             errors.rejectValue("nickName", "", "Name must contain only a-z A-Z 0-9 _ + * / -");
         }
-        if (!registrationForm.getPassword().equals(registrationForm.getPasswordRepeat())) {
+    }
+
+    private void passwordMatcher(Errors errors,RegistrationForm registrationForm){
+        if (isPasswordsNotEquals(registrationForm)) {
             errors.rejectValue("password", "", "Passwords didn't match");
             errors.rejectValue("passwordRepeat", "", "Passwords didn't match");
         }
+    }
 
+    private boolean isPasswordsNotEquals(RegistrationForm registrationForm){
+        return !registrationForm.getPassword().equals(registrationForm.getPasswordRepeat());
+    }
+
+
+    private void validateEmptyOrWhitespace(Errors errors){
         String emptyError = "Can't be empty";
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "", emptyError);
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "nickName", "", emptyError);
